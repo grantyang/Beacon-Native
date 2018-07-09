c2aeb434ccf11a667282d57b42880bbc20ebf683
Grant Yang <grantyang1@gmail.com>
2018-07-09 11:45:41 -0700

    Adds basic map functionality

--- native/components/Map.js
+++ native/components/Map.js
@@ -5,14 +5,33 @@ import Mapbox from '@mapbox/react-native-mapbox-gl';
 Mapbox.setAccessToken('pk.eyJ1IjoiZ3JhbnR5YW5nMSIsImEiOiJjampjM3lxMjkyMHB4M3dvaWxxOXA2cTBjIn0.R4msLIMtjPSuW6op7aC9ng');
 
 export default class Map extends Component {
+
+  renderAnnotations () {
+    return (
+      <Mapbox.PointAnnotation
+        key='pointAnnotation'
+        id='pointAnnotation'
+        coordinate={[-122.4194, 37.7749]}>
+
+        <View style={styles.annotationContainer}>
+          <View style={styles.annotationFill} />
+        </View>
+        <Mapbox.Callout title='Look! An annotation!' />
+      </Mapbox.PointAnnotation>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Mapbox.MapView
             styleURL={Mapbox.StyleURL.Street}
-            zoomLevel={15}
-            centerCoordinate={[11.256, 43.770]}
+            zoomLevel={11}
+            centerCoordinate={[-122.4194, 37.7749]}
+            showUserLocation={true}
             style={styles.container}>
+            
+          {this.renderAnnotations()}
         </Mapbox.MapView>
       </View>
     );
@@ -22,6 +41,21 @@ export default class Map extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 200,
+    width: 375,
   },
+  annotationContainer: {
+    width: 20,
+    height: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+  annotationFill: {
+    width: 20,
+    height: 20,
+    borderRadius: 15,
+    backgroundColor: 'purple',
+    transform: [{ scale: 0.6 }],
+  }
 });
